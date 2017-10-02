1bd222f36f8aa18768e8e0f07f96bce3c4c81103
srmcderm <fv4105@wayne.edu>
2017-10-02 13:44:07 -0400

    add wow button for my self esteem

--- window.py
+++ window.py
@@ -1,5 +1,7 @@
 from tkinter import Tk, Label, Button
 
+#Window with a label and two buttons
+
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
@@ -8,14 +10,20 @@ class MyFirstGUI:
         self.label = Label(master, text='This is our first GUI')
         self.label.pack()
         
-        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button = Button(master, text='Greet', command=self.greet)
         self.greet_button.pack()
         
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.wow_button = Button(master, text='Wow', command=self.wow)
+        self.wow_button.pack()
+        
+        self.close_button = Button(master, text='Close', command=master.quit)
         self.close_button.pack()
         
     def greet(self):
         print("Greetings!")
+    
+    def wow(self):
+        print("Wow, you're amazing AND beautiful. Keep up the good work.")
 
 root = Tk()
 my_gui = MyFirstGUI(root)
